f3e4d9687baeecf1227cfe2a596712f3935eb59d
Cleboost <clement.balarot@gmail.com>
2022-02-01 18:53:51 +0100

    Ajout du bouton + Modif langue
    Changement de la langue + ajoute du bouton nous contactez.

--- header.php
+++ header.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -9,6 +9,6 @@
 </head>
 <body>
     <button class="buttonPlay">Jouer</button>
-    
+    <p>Nous contactez</p>
 </body>
 </html>
