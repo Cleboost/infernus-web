bcf6ae72d04e4f64d7c193076b0bc8d9c67f955e
Cleboost <clement.balarot@gmail.com>
2022-02-01 13:33:46 +0100

    base

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Infernus</title>
+    <link rel="icon" href="images/icon.ico">
+</head>
+<body>
+    <?php include("header.php") ?>
+    <h1>Infernus</h1>
+    
+</body>
+</html>
